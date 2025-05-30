ff42ebd78d0a037d818cb831557416a19929e917
Fathi <chafaii.fathi@gmail.com>
2025-05-30 11:29:19 +0100

    refactor: pass params to client constructor

--- cmd/go-chat-client/main.go
+++ cmd/go-chat-client/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/Sp33ktrE/chat-cli/internal/client"
 )
 
+const HOST = ""
+const PORT = "5050"
+
 func main() {
-	client, err := client.New()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println(">> Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	user, err := client.New(name, fmt.Sprintf("%s:%s", HOST, PORT))
 	if err != nil {
 		log.Fatal(err)
 	}
-	client.Chat()
+	user.Chat()
 }
